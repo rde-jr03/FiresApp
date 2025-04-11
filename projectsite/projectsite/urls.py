575a4abfa5e31e89ce9955f2534f08aa5e916fb5
[rde-jr03] <[ronnieevangelistajr@gmail.com]>
2025-04-11 18:51:12 +0800

    fire incident map and charts

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity, map_station, fire_incident_map
 from fire import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('stations', views.map_station, name='map-station'),
     path('', HomePageView.as_view(), name='home'),
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
+    path('stations/', map_station, name='map-station'),  
+    path('fire_incident_map/', fire_incident_map, name='fire-incident-map'),
 
 ]
