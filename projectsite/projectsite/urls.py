141b74500666a834b0b39c18c306fc61a80be771
[rde-jr03] <[ronnieevangelistajr@gmail.com]>
2025-04-11 18:19:28 +0800

    fire stations map

---
+++ projectsite/projectsite/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from fire.views import HomePageView
+from fire import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('stations', views.map_station, name='map-station'),
+    path('', HomePageView.as_view(), name='home'),
+
+]
